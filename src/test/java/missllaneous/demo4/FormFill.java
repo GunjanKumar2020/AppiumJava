eb806c5fe9156cfaedcf8b0c94564291de7f5c13
Gunjan Kumar <gunjan.kmr0019@gmail.com>
2026-06-03 23:08:51 +0530

    Added a FormFill Class

---
+++ src/test/java/missllaneous/demo4/FormFill.java
@@ -0,0 +1,13 @@
+package missllaneous.demo4;
+
+import org.testng.annotations.Test;
+
+public class FormFill extends BaseScreen 
+{
+    @Test
+    public void validateformFill()
+    {
+        // code will be added later
+    }
+
+}
