4f8ad8524cf0419805469fc9c76467f983dd2588
Gunjan Kumar <gunjan.kmr0019@gmail.com>
2026-06-03 14:34:29 +0530

    DragAndDrop Class is created

---
+++ src/test/java/missllaneous/demo4/DragAndDrop.java
@@ -0,0 +1,13 @@
+package missllaneous.demo4;
+
+import org.testng.annotations.Test;
+
+public class DragAndDrop extends BaseScreen
+{
+ 
+	@Test
+	public void validateDragAndDropFunctionality()
+	{
+		// Code will be added soon.
+	}
+}
