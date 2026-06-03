eb5136bdfc05bc9e334133a40b165b99731ef7e7
Gunjan Kumar <gunjan.kmr0019@gmail.com>
2026-06-03 14:48:47 +0530

    Added DragAndDrop Class

---
+++ src/test/java/missllaneous/demo4/DragAndDrop.java
@@ -0,0 +1,12 @@
+package missllaneous.demo4;
+
+import org.testng.annotations.Test;
+
+public class DragAndDrop extends BaseScreen 
+{
+ @Test
+ public void validateDragAndDrop()
+ {
+	 // Code will be added soon
+ }
+}
